b79406ca600dcacb88e9733a0a2575576926d504
Prakash Kumar Badaila <badailap928@gmail.com>
2026-04-27 16:16:07 +0545

    login page is setup

--- frontend/vite-project/src/pages/LoginSignup.jsx
+++ frontend/vite-project/src/pages/LoginSignup.jsx
@@ -1,26 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../CSS/LoginSignup.css"
+
 const LoginSignup = () => {
+
+  const [action, setAction] = useState("Sign Up");
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+
+        <h1>{action}</h1>
+
         <div className='loginsignup-fields'>
-          <input type='text' placeholder='First Name'/>
-           <input type='text' placeholder='Last Name'/>
-          <input type='email' placeholder='Email Address'/>
-          <input type='password' placeholder='Password'/>
-          <input type="number" placeholder='Phone no.' />
-          <button>Continue</button>
+
+          {action === "Sign Up" && (
+            <>
+              <input type='text' placeholder='First Name' />
+              <input type='text' placeholder='Last Name' />
+              <input type="number" placeholder='Phone No.' />
+            </>
+          )}
+
+          <input type='email' placeholder='Email Address' />
+          <input type='password' placeholder='Password' />
+
         </div>
-        
-        <p className="loginsignup-login">
-          Already have an account ? <span>Login here</span>
-        </p>
-        <div className='loginsignup-agree'>
-          <input type='checkbox' name='' id=''/>
-          <p>By continuing, i agree to the terms of use & privacy policy.</p>
-        </div>
+
+        <button>
+          {action === "Sign Up" ? "Create Account" : "Login"}
+        </button>
+
+        {/* TOGGLE TEXT */}
+        {action === "Sign Up" ? (
+          <p className="loginsignup-login">
+            Already have an account?{" "}
+            <span onClick={() => setAction("Login")}>Login here</span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Don’t have an account?{" "}
+            <span onClick={() => setAction("Sign Up")}>Sign up here</span>
+          </p>
+        )}
+
+        {/* AGREEMENT only for signup */}
+        {action === "Sign Up" && (
+          <div className='loginsignup-agree'>
+            <input type='checkbox' />
+            <p>By continuing, I agree to the terms & privacy policy.</p>
+          </div>
+        )}
+
       </div>
     </div>
   )
